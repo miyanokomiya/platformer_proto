062939075269b68ac81f9c00984c0b10064e51ab
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-16 22:27:27 +0900

    jump action

--- scenes/pawns/player/player.gd
+++ scenes/pawns/player/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer
 
 const SPEED = 120.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
+
+enum PLAYER_STATE{GROUND, AIR}
+var current_state = PLAYER_STATE.GROUND
 
 var current_direction = 1
 
@@ -12,27 +15,60 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	match current_state:
+		PLAYER_STATE.GROUND:
+			state_process_ground(delta)
+		PLAYER_STATE.AIR:
+			state_process_air(delta)
+	
+	move_and_slide()
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+func free_move() -> bool:
+	var direction = sign(Input.get_axis("move_left", "move_right"))
 	if direction:
 		velocity.x = direction * SPEED
-		animation_player.play("run")
+		if is_on_floor():
+			animation_player.play("run")
+		
 		if current_direction * direction < 0:
 			scale.x *= -1
 			
 		current_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation_player.play("idle")
 	
+	return !!direction
 
-	move_and_slide()
+
+func state_process_ground(delta):
+	if !is_on_floor():
+		current_state = PLAYER_STATE.AIR
+		return
+	
+	if Input.is_action_just_pressed("action_jump"):
+		velocity.y = JUMP_VELOCITY
+		state_process_air(delta)
+		return
+		
+	if free_move():
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
+
+
+func state_process_air(delta):
+	if is_on_floor():
+		current_state = PLAYER_STATE.GROUND
+		return
+	
+	velocity.y += gravity * delta
+	
+	if velocity.y < JUMP_VELOCITY / 8:
+		animation_player.play("jump_up")
+	elif velocity.y < 200:
+		animation_player.play("jump_high")
+	else:
+		animation_player.play("jump_fall")
+	
+	free_move()
