9aeb1133450f1892ed65b94d8a7b485b74748217
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-22 19:43:58 +0900

    camera bounds area

---
+++ scenes/field_objects/camera_bounds/camera_bounds.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@export var camera: MainCamera
+
+@onready var bounds_area = %BoundsArea
+
+
+func _ready():
+	body_entered.connect(on_body_entered)
+	body_exited.connect(on_body_existed)
+
+
+func set_bounds():
+	if !camera:
+		return
+	
+	var bounds_center = bounds_area.global_position
+	var bounds_size = (bounds_area.shape as RectangleShape2D).size
+	camera.set_bounds(get_instance_id(), bounds_center, bounds_size)
+
+
+func reset_bounds():
+	if !camera:
+		return
+	
+	if camera.limit_id != get_instance_id():
+		return
+	
+	camera.restore_original_limit()
+
+
+func on_body_entered(_body: Node2D):
+	set_bounds()
+
+
+func on_body_existed(_body: Node2D):
+	reset_bounds()
