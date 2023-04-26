2bbd6e0f5b426d30a1e392ec6fc1081266d85db0
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-26 21:04:51 +0900

    Auto facing toward the player on spawning

--- scenes/field_objects/on_screen_spawner/on_screen_spawner.gd
+++ scenes/field_objects/on_screen_spawner/on_screen_spawner.gd
@@ -1,8 +1,9 @@
 extends VisibleOnScreenNotifier2D
 
-enum DIRECTION{DEFAULT, FLIP, AUTO}
+enum DIRECTION{DEFAULT, FLIP}
 
 @export var pawn_scene: PackedScene
+@export var facing: bool = true
 @export var direction_x: DIRECTION = DIRECTION.DEFAULT
 @export var direction_y: DIRECTION = DIRECTION.DEFAULT
 
@@ -24,12 +25,32 @@ func spawn_children():
 	var pawn = pawn_scene.instantiate()
 	if pawn is EnemyBase:
 		var enemy = pawn as EnemyBase
-		if direction_x == DIRECTION.FLIP:
+		var d = get_direction()
+		if d.x < 0:
 			enemy.turn_h()
-		if direction_y == DIRECTION.FLIP:
+		if d.y < 0:
 			enemy.turn_v()
 	
 	add_child(pawn)
+
+
+func get_direction() -> Vector2:
+	var v = Vector2(1, 1)
+	if facing:
+		var player = get_tree().get_first_node_in_group("Player") as Node2D
+		if player:
+			# Enemies are supposed to face toward left hand side
+			v.x = player.global_position.direction_to(global_position).x
+	
+	match direction_x:
+		DIRECTION.FLIP:
+			v.x *= -1
+	
+	match direction_y:
+		DIRECTION.FLIP:
+			v.y *= -1
+	
+	return v
 
 
 func _on_screen_entered():
