92efa580c578dcd57823c2538d1371689d848079
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-30 17:17:27 +0900

    Polish some items

--- scenes/components/teleport_component/teleport_component.gd
+++ scenes/components/teleport_component/teleport_component.gd
@@ -6,6 +6,8 @@ signal finished
 @export var sprite: Sprite2D
 @export var teleport_material: ShaderMaterial
 
+@onready var audio_stream_player = $AudioStreamPlayer
+
 var tween_obj: Tween
 
 
@@ -14,6 +16,7 @@ func play():
 	if tween_obj != null && tween_obj.is_valid():
 		tween_obj.kill()
 	
+	play_se()
 	teleport_material.set_shader_parameter("progress", 1.0)
 	tween_obj = create_tween()
 	tween_obj.tween_property(teleport_material, "shader_parameter/progress", 0.0, duration)
@@ -26,8 +29,14 @@ func play_backwards():
 	if tween_obj != null && tween_obj.is_valid():
 		tween_obj.kill()
 	
+	play_se()
 	teleport_material.set_shader_parameter("progress", 0.0)
 	tween_obj = create_tween()
 	tween_obj.tween_property(teleport_material, "shader_parameter/progress", 1.0, duration)
 	await tween_obj.finished
 	finished.emit()
+
+
+func play_se():
+	await get_tree().create_timer(0.2).timeout
+	audio_stream_player.play()
