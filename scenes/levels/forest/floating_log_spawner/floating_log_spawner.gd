37110347becfeb2c8d85ea2e2da8b721c39941b5
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-05-14 12:23:01 +0900

    Polish cliff fortress

--- scenes/levels/forest/floating_log_spawner/floating_log_spawner.gd
+++ scenes/levels/forest/floating_log_spawner/floating_log_spawner.gd
@@ -5,6 +5,8 @@ extends Node2D
 @export var delay: float = 0.0
 @export var log_lifetime: float = 20.0
 @export var log_speed: float = 20.0
+## Used to spawn initial logs
+@export var prespawn_count: int = 0
 @export var disabled: bool = false
 
 @onready var timer = $Timer
@@ -18,17 +20,26 @@ func _ready():
 	if delay > 0.0:
 		await get_tree().create_timer(delay).timeout
 	
+	for i in prespawn_count:
+		Callable(prespawn).call_deferred(i)
+	
 	timer.start()
 
 
-func spawn():
-	var floating_log = floating_log_scene.instantiate() as Node2D
+func spawn() -> CharacterBody2D:
+	var floating_log = floating_log_scene.instantiate() as CharacterBody2D
 	floating_log.global_position = global_position
 	floating_log.lifetime = log_lifetime
 	floating_log.speed = log_speed
 	var layer = get_tree().get_first_node_in_group("background_layer")
 	layer = layer if layer else self
 	layer.add_child(floating_log)
+	return floating_log
+
+
+func prespawn(index: int):
+	var floating_log = spawn()
+	floating_log.global_position.y += index * log_speed * duration
 
 
 func _on_timer_timeout():
