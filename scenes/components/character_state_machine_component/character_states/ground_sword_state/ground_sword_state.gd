478ff5be940d51ccf4d48a5174cc5318a59ef599
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-05-17 19:22:01 +0900

    Ground sword 2

--- scenes/components/character_state_machine_component/character_states/ground_sword_state/ground_sword_state.gd
+++ scenes/components/character_state_machine_component/character_states/ground_sword_state/ground_sword_state.gd
@@ -1,20 +1,30 @@
 extends CharacterGroundState
 
 @onready var timer = $Timer
+@onready var combo_1_timer = $Combo1Timer
 
 var dash_pressed = false
 var jump_pressed = false
+var sword_pressed_count = 0
+var sword_count = 0
 var initial_x_velocity = 0.0
 
 
 func on_enter(ctx: CharacterStateContext):
+	sword_count += 1
 	ctx.animation_player.play("ground_sword_1")
 	ctx.has_dash_momentum = false
 	ctx.set_after_effect_playing(false)
-	timer.start()
 	dash_pressed = false
 	jump_pressed = false
+	sword_pressed_count = 0
 	initial_x_velocity = abs(ctx.character.velocity.x)
+	combo_1_timer.start()
+	init()
+
+
+func init():
+	timer.start()
 
 
 func state_process(ctx: CharacterStateContext, delta: float):
@@ -37,8 +47,22 @@ func state_process(ctx: CharacterStateContext, delta: float):
 	if Input.is_action_just_pressed("action_jump"):
 		jump_pressed = true
 	
+	if Input.is_action_just_pressed("action_weapon"):
+		sword_pressed_count += 1
+		# Override other action buffers
+		dash_pressed = false
+		jump_pressed = false
+	
 	if !timer.is_stopped():
 		return
+	
+	if sword_pressed_count > 0 && combo_1_timer.is_stopped():
+		if sword_count == 1:
+			sword_count += 1
+			sword_pressed_count -= 1
+			ctx.animation_player.play("ground_sword_2")
+			init()
+			return
 	
 	if !ctx.animation_player.is_playing():
 		next_state_name = "ground"
